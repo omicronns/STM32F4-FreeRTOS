bff031b01ed680e475900d4fc9e1cb440af464ea
omicronns <omicronns@gmail.com>
2015-05-19 20:18:50 +0200

    init

--- main.c
+++ main.c
@@ -10,24 +10,25 @@ void init_USART3(void);
 void test_FPU_test(void* p);
 
 int main(void) {
-  uint8_t ret;
+    uint8_t ret;
 
-  SystemInit();
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-  init_USART3();
+    SystemInit();
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+    init_USART3();
 
-  // Create a task
-  ret = xTaskCreate(test_FPU_test, "FPU", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    // Create a task
+    ret = xTaskCreate(test_FPU_test, "FPU", configMINIMAL_STACK_SIZE + 100, NULL, 1, NULL);
 
-  if (ret == pdTRUE) {
-    printf("System Started!\n");
-    vTaskStartScheduler();  // should never return
-  } else {
-    printf("System Error!\n");
-    // --TODO blink some LEDs to indicate fatal system error
-  }
+    if(ret == pdTRUE) {
+        printf("System Started!\n");
+        vTaskStartScheduler();  // should never return
+    }
+    else {
+        printf("System Error!\n");
+        // --TODO blink some LEDs to indicate fatal system error
+    }
 
-  for (;;);
+    for(;;);
 }
 
 void vApplicationTickHook(void) {
@@ -44,8 +45,8 @@ void vApplicationTickHook(void) {
    to query the size of free heap space that remains (although it does not
    provide information on how the remaining heap might be fragmented). */
 void vApplicationMallocFailedHook(void) {
-  taskDISABLE_INTERRUPTS();
-  for(;;);
+    taskDISABLE_INTERRUPTS();
+    for(;;);
 }
 
 /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
@@ -61,54 +62,52 @@ void vApplicationIdleHook(void) {
 }
 
 void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName) {
-  (void) pcTaskName;
-  (void) pxTask;
-  /* Run time stack overflow checking is performed if
+    (void) pcTaskName;
+    (void) pxTask;
+    /* Run time stack overflow checking is performed if
      configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
      function is called if a stack overflow is detected. */
-  taskDISABLE_INTERRUPTS();
-  for(;;);
+    taskDISABLE_INTERRUPTS();
+    for(;;);
 }
 
 void test_FPU_test(void* p) {
-  float ff = 1.0f;
-  printf("Start FPU test task.\n");
-  for (;;) {
-    float s = sinf(ff);
-    ff += s;
-    // TODO some other test
-    vTaskDelay(1000);
-  }
-  vTaskDelete(NULL);
+    int iter = 0;
+    printf("Start FPU test task.\n");
+    for(;;) {
+        printf("Iteration:%d sin:%f\n", iter++, sinf((float)iter));
+        vTaskDelay(1000);
+    }
+    vTaskDelete(NULL);
 }
 
 /*
  * Configure USART3(PB10, PB11) to redirect printf data to host PC.
  */
 void init_USART3(void) {
-  GPIO_InitTypeDef GPIO_InitStruct;
-  USART_InitTypeDef USART_InitStruct;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    USART_InitTypeDef USART_InitStruct;
 
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOB, &GPIO_InitStruct);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
-  GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
 
-  USART_InitStruct.USART_BaudRate = 115200;
-  USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-  USART_InitStruct.USART_StopBits = USART_StopBits_1;
-  USART_InitStruct.USART_Parity = USART_Parity_No;
-  USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-  USART_Init(USART3, &USART_InitStruct);
-  USART_Cmd(USART3, ENABLE);
+    USART_InitStruct.USART_BaudRate = 115200;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    USART_Init(USART3, &USART_InitStruct);
+    USART_Cmd(USART3, ENABLE);
 }
 
